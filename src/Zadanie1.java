c70cc15aa57f4c81eb4b13d66d4ead83f296cfd3
Sryncu <Sryncu@Sryncu-Komputer>
2017-12-02 15:12:21 +0100

    Iteracyjna

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
 
 public class Zadanie1 {
 
 	public static void main(String[] args) {
+	
+		System.out.print("Podaj cyfrê:");
 		
+		Scanner odczyt = new Scanner(System.in);
 		
+		int liczba = odczyt.nextInt();
+		int silnia=1, j=1;
+
+		for(int i=1; i<=liczba; i++) {
+			
+			silnia = silnia*i;
+			
+		}
 		
-		
-		
+		System.out.print(silnia);
+			
 	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
